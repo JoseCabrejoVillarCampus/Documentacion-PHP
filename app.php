09f4d1d42bf7764dba27c94deaf7b1eb2b18689f
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-26 13:29:28 -0500

    feat: :sparkles: Funciones isset() y empty()

--- app.php
+++ app.php
@@ -1,65 +1,19 @@
 <?php
 /*
-TODO: Arreglos, Arreglos Asociativos y Funciones Para Arreglos  
-*/
-/*
-?Los arreglos en php son estructuras, donde podemos almacenar multiples tipos de datos y de valor, para anexar un arregl lo podemos hacer de la siguiente manera:  
-*/
-$arreglo= array("valor1","valor2","valor3");
-/*
-*Acceder a un elemento del array
-*/
-$arreglo[1];
-/*
-*Añadir un elemento en un indice especifico del arreglo  
-*/
-$arreglo[3]='elemento';
-/*
-*Añadir un elemento al final del Array
-*/
-array_push($arreglo,'elemento1');
-/*
-*Añadir elemento al inicio
-*/
-array_unshift($arreglo,'elemento2');
-/*
-*Para ver los elementos dentro de un array  
-*/
-var_dump($arreglo);
-/*
-?Array Asociativos: Son un tipo de estructura de datos que nos permiten asociar claves a los valores  
-*/
-$arregloAsociativo=array(
-    "calve1"=>"valor1",
-    "calve2"=>"valor2",
-    "calve3"=>"valor3"
-);
-/*
-*Para acceder a un valor de un array asociativo  
-*/
-echo $arregloAsociativo["clave1"];
-/*
-?Funciones en arreglos:  
+TODO: Isset()  
 */
 /*  
-*in_array(): Buscar elementos en un array
+?La función isset() comprueba si una variable o un elemento de un array está definido y no es null.
+?Esta función devuelve true si la variable o el elemento de array existe y tiene un valor, y false en caso
+?contrario
 */
-/*  
-*sort(): Oredenar elementos en un array de menor a mayor
+/*
+TODO: Empty()  
 */
-/*  
-*rsort(): Ordena elementos en un array de mayor a menor
+/*
+?la función empty() comprueba si una variable o un elemento de un array está vacío.
+?Esta función devuelve true si la variable o el elemento de array no tiene un valor definido, o si tiene
+?un valor que se considera vacío (por ejemplo, una cadena vacía, 0, false, null, un array vacío, entre
+?otros), y false en caso contrario
 */
-/*  
-*asort(): Ordena elementos en un array por orden alfabetico
-*/
-/*  
-*arsort(): Ordena por orden alfabetico empezando por la z
-*/
-/*  
-*ksort(): Ordena por las llaves en orden alfabetico
-*/
-/*  
-*krsort(): Ordena por las llaves en orden alfabetico de la z a la a
-*/
-?>
+
