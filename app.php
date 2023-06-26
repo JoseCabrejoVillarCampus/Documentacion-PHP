2f56da533422ee0dfa618297ab6c63709bafc3a6
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-26 17:15:44 -0500

    feat: :sparkles: json_encode y json_decode
    funciones para transformar datos json

--- app.php
+++ app.php
@@ -1,77 +1,15 @@
 <?php
 /*
-TODO: Funciones Con Array  
+TODO: Json_encode  
 */
 /*  
-?array_chunk = Divide el array principal en las partes que deseemos, el argumento es el numero de elementos
+?Se usa para convertir una estructura de datos en php en una cadena JSON
 */
-$funcion = array_chunk($array,2);
+$message = json_encode(["Code" => 200 + $res->rowCount(), "Message" => "inserted data"]);
+/*
+TODO: Json_decode  
+*/
 /*  
-?array_slice = Obtenemos un nuevo array dentro de los parametros que deseamos a partir del argumento
+?Se usa para convertir una cadena JSON en una estructura de datos de php
 */
-$funcion = array_slice($array,4);
-/*  
-?array_merge = Combinamos dos arrays en uno solo
-*/
-$funcion = array_merge($array1,$array2);
-/*  
-?array_pop = Elimina el ultimo elemento de un array
-*/
-$funcion = array_pop($ultimoelemento);
-/*  
-?array_serch = nor permite buscar dentro de un array
-*/
-$funcion = array_search('elemento',$array);
-/*  
-?array_reverse = invierte el array
-*/
-$funcion = array_reverse($array);
-/*
-?array_flip(): Intercambia las claves con sus valores correspondientes en un array.  
-*/
-/*
-?array_fill(): Rellena un array con un valor especificado.
-*/
-/*
-?array_filter(): Filtra los elementos de un array utilizando una función de devolución de llamada.
-*/
-/*
-?array_map(): Aplica una función a cada elemento de un array y devuelve un nuevo array con los resultados.
-*/
-/*
-?array_reduce(): Reduce un array a un solo valor aplicando una función de devolución de llamada.
-*/
-/*
-?array_key_exists(): Comprueba si una clave existe en un array.
-*/
-/*
-?array_rand(): Devuelve una o varias claves aleatorias de un array.
-*/
-/*
-?array_unique(): Elimina los valores duplicados de un array.
-*/
-/*
-?array_intersect(): Devuelve un array con los valores comunes a todos los arrays dados.
-*/
-/*
-?array_diff(): Devuelve un array con los valores del primer array que no están presentes en los arrays adicionales.
-*/
-/*
-?array_push(): Agrega uno o más elementos al final de un array.
-*/
-/*
-?array_sum(): Devuelve la suma de todos los valores de un array numérico.
-*/
-/*
-?array_product(): Devuelve el producto de todos los valores de un array numérico.
-*/
-/*
-?array_keys(): Devuelve todas las claves de un array.
-*/
-/*
-?array_values(): Devuelve todos los valores de un array.
-*/
-/*
-?array_walk(): Aplica una función de devolución de llamada a cada elemento de un array.
-*/
-
+\App\academic_area\academic_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAcademicArea();
