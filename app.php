b892e356d844cb3d89ba6373f4598750a0cd0ad5
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 17:34:08 -0500

    feat: :sparkles: Propiedades

--- app.php
+++ app.php
@@ -1,7 +1,13 @@
 <?php
 /*
-TODO: ::class 
+TODO: Propiedades 
 */
 /*  
-?Esta palabra reservada se usa para la reslucion de nombre de clases, es una transformacion durante la compilacion, donde se obtiene un string con el nombre completamente cualificado de la clase
+?Son las variables pertenecientes a una clase, tambien llamadas atributos o campos. Se definen usando palabras reservadas Public, Protected o Private.
+?Dentro de los metodos de una clase, se puede acceder a las propiedades no estaticas utilizando ->(operador de objeto)
 */
+$this->propiedad;
+/*  
+? A las propiedades estaticas se puede acceder utilizando :: (doble punto)
+*/
+self::propiedad
