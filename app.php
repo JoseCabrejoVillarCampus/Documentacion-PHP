61aed1a2e5c8201f204bc67a22f2aeada22731e4
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 06:13:36 -0500

    feat: :sparkles: parent::

--- app.php
+++ app.php
@@ -1,14 +1,20 @@
 <?php
 /*
-TODO: Extends  
+TODO: parent::  
 */
 /*  
-?Una clase puede heredar los metodos y propiedades de otra clase usando la palabra reservada extends en la declaracion de la clase.
-
-?Los metodos y propiedades heredados pueden ser sobreescritos con la redeclaracion de estos utilizando el mismo nombre que en la clase madre, a menos que se halla definido el metodo como final.
+?Es posible acceder a los metodos sobreescritos o a las propiedades estaticas haciendo referencia a ellos con
 */
 class BaseClass{
     final public function test(){
         echo 'llamada';
     }
 }
+class ClassExtendida extends BaseClass{
+    function mostrarVar(){
+        echo 'clase extendida';
+        parent::mostrarVar();
+    }
+}
+$extendida =  new ClassExtendida();
+$extendida->mostrarVar();
