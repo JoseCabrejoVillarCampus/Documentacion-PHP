11d7de0c70c8bc7a7b43b04e98d03a76bada38e7
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 06:06:19 -0500

    feat: :sparkles: Extends

--- app.php
+++ app.php
@@ -1,7 +1,14 @@
 <?php
 /*
-TODO: Propiedades y Metodos  
+TODO: Extends  
 */
 /*  
-?Las propiedades y los metodos se encuentran en espacios diferentes, por lo tanto es posible tener una propiedad y un metodocon el mismo nombre. Su acceso solo depende del contexto, es decir si se emplea una variable o una funcion
+?Una clase puede heredar los metodos y propiedades de otra clase usando la palabra reservada extends en la declaracion de la clase.
+
+?Los metodos y propiedades heredados pueden ser sobreescritos con la redeclaracion de estos utilizando el mismo nombre que en la clase madre, a menos que se halla definido el metodo como final.
 */
+class BaseClass{
+    final public function test(){
+        echo 'llamada';
+    }
+}
