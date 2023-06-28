ab9527ba0ebefdaa6b865cc59cf9247e33d1b3ea
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 21:24:49 -0500

    feat: :sparkles: Namespaces en PHP

--- app.php
+++ app.php
@@ -1,17 +1,27 @@
 <?php
 /*
-TODO: Autoload 
+TODO: Namespaces en PHP 
 */
 /*  
-?es una técnica que permite cargar automáticamente las
-?clases cuando son necesarias, sin tener que incluir manualmente los archivos de clase en cada punto
-?del código.
+?Usamos la palabra namespace para
+?importar clases, funciones y constantes desde otros espacios de nombres.
 */
 // Clase base
+namespace App;
+
+class Animal{
+    public function __construct(){
+        echo"perro";
+    }
+}
+/*  
+?Cuando se utiliza use en la definición de un espacio de nombres, se está especificando una ruta corta
+?(alias) para acceder a un elemento específico de otro espacio de nombres.
+*/
+use App\Animal;
 function my_autoload($clase){
-    require __DIR__.'/clases/'.$clase.'.php';
+    $fileClass = explode('\\',$clase);
+    require __DIR__.'/clases/'.$fileClass[1].'.php';
 }
 spl_autoload_register('my_autoload');
-
-$nombre= new Nombre();
-$edad= new Edades();
+$animal= new Animal();
