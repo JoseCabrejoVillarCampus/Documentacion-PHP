f44e2a2ef3350d04155122be1e465e95754e1135
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 21:36:03 -0500

    feat: :sparkles: Base de Datos Relacionales

--- app.php
+++ app.php
@@ -1,8 +1,24 @@
 <?php
 /*
-TODO: Composer 
+TODO: Base de Datos Relacionales 
 */
 /*  
-?Composer es un administrador de dependencias para PHP que permite instalar librerías de terceros
-?para facilitar el desarrollo con php. Componser es el equivalente a npm para node JS.
+?Las bases de datos relacionales son un tipo de sistema de gestión de bases de datos (SGBD) que
+?organiza la información en tablas estructuradas y establece relaciones entre ellas. Estas bases de datos
+?se basan en el modelo relacional propuesto por Edgar Codd en la década de 1970.
+?as relaciones entre las tablas se establecen mediante claves primarias y claves foráneas. Una clave
+?primaria es un atributo o conjunto de atributos que identifica de forma única cada fila en una tabla.
+?Una clave foránea es un atributo en una tabla que hace referencia a la clave primaria de otra tabla,
+?estableciendo así una relación entre ellas. Estas relaciones permiten conectar la información entre
+?diferentes tablas y realizar consultas y operaciones complejas.
+
+
+
+?Un sistema de gestión de bases de datos relacionales (RDBMS) almacena los datos en tablas. Cada
+?tabla recibe un nombre por parte del usuario que la crea. El usuario suele elegir un nombre
+?relacionado con los datos que se almacenarán en la tabla; por ejemplo, STUDENTS, EMPLOYEES,
+?LOCATIONS. Cuando se crea una tabla, el usuario también crea y nombra las columnas relacionadas
+?con las características específicas que se almacenan para cada registro.
+
+
 */
