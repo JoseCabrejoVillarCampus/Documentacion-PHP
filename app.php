069e7227970cd60e5f3a93a5d59d2f9d6c550ec1
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 21:30:11 -0500

    feat: :sparkles: Composer

--- app.php
+++ app.php
@@ -1,27 +1,8 @@
 <?php
 /*
-TODO: Namespaces en PHP 
+TODO: Composer 
 */
 /*  
-?Usamos la palabra namespace para
-?importar clases, funciones y constantes desde otros espacios de nombres.
+?Composer es un administrador de dependencias para PHP que permite instalar librerías de terceros
+?para facilitar el desarrollo con php. Componser es el equivalente a npm para node JS.
 */
-// Clase base
-namespace App;
-
-class Animal{
-    public function __construct(){
-        echo"perro";
-    }
-}
-/*  
-?Cuando se utiliza use en la definición de un espacio de nombres, se está especificando una ruta corta
-?(alias) para acceder a un elemento específico de otro espacio de nombres.
-*/
-use App\Animal;
-function my_autoload($clase){
-    $fileClass = explode('\\',$clase);
-    require __DIR__.'/clases/'.$fileClass[1].'.php';
-}
-spl_autoload_register('my_autoload');
-$animal= new Animal();
