ae371f4a59581103fb1675bf544e7fbd66e4d2cb
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 21:39:36 -0500

    feat: :sparkles: SQL Structured Query Language

--- app.php
+++ app.php
@@ -1,24 +1,26 @@
 <?php
 /*
-TODO: Base de Datos Relacionales 
+TODO: SQL Structured Query Language 
 */
 /*  
-?Las bases de datos relacionales son un tipo de sistema de gestión de bases de datos (SGBD) que
-?organiza la información en tablas estructuradas y establece relaciones entre ellas. Estas bases de datos
-?se basan en el modelo relacional propuesto por Edgar Codd en la década de 1970.
-?as relaciones entre las tablas se establecen mediante claves primarias y claves foráneas. Una clave
-?primaria es un atributo o conjunto de atributos que identifica de forma única cada fila en una tabla.
-?Una clave foránea es un atributo en una tabla que hace referencia a la clave primaria de otra tabla,
-?estableciendo así una relación entre ellas. Estas relaciones permiten conectar la información entre
-?diferentes tablas y realizar consultas y operaciones complejas.
+?LSQL (Structured Query Language) es un lenguaje de programación utilizado para administrar y
+?manipular bases de datos relacionales. Fue desarrollado en la década de 1970 y se ha convertido en
+?el estándar de facto para trabajar con bases de datos.
+?SQL permite a los usuarios crear, modificar y eliminar bases de datos, así como realizar consultas
+?para recuperar información específica de una base de datos
 
+!El lenguaje SQL consta de varios comandos, entre los que se incluyen:
 
-
-?Un sistema de gestión de bases de datos relacionales (RDBMS) almacena los datos en tablas. Cada
-?tabla recibe un nombre por parte del usuario que la crea. El usuario suele elegir un nombre
-?relacionado con los datos que se almacenarán en la tabla; por ejemplo, STUDENTS, EMPLOYEES,
-?LOCATIONS. Cuando se crea una tabla, el usuario también crea y nombra las columnas relacionadas
-?con las características específicas que se almacenan para cada registro.
-
+?DDL (Data Definition Language): Utilizado para definir y modificar la estructura de la base de
+?datos. Incluye comandos como CREATE, ALTER y DROP para crear, modificar y eliminar tablas,
+?índices, vistas, etc.
+*DML (Data Manipulation Language): Utilizado para manipular los datos almacenados en la
+*base de datos. Incluye comandos como INSERT, UPDATE y DELETE para agregar, actualizar y
+*eliminar registros.
+?DQL (Data Query Language): Utilizado para realizar consultas y recuperar información de la
+?base de datos. El comando más común es SELECT, que permite especificar los criterios de
+?búsqueda y los campos a recuperar.
+*DCL (Data Control Language): Utilizado para controlar los privilegios de acceso a la base de
+*datos. Incluye comandos como GRANT y REVOKE para otorgar y revocar permisos.
 
 */
